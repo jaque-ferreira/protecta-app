935466c6adead299f04fa52b0a4fa4bd6676898c
jaque-ferreira <jaqueline.s.ferreira@edu.ufes.br>
2024-03-04 16:44:13 -0300

    botão de instalação

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,44 @@ if ('serviceWorker' in navigator) {
     .then((reg) => console.log('sw registrado', reg))
     .catch((err) => console.log('sw não registrado', err));
 }
+
+    // main.js
+
+    let installPrompt = null;
+    const installButton = document.querySelector("#btn-instalar");
+    
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      installPrompt = event;
+      installButton.removeAttribute("hidden");
+    });
+
+
+    // main.js
+
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) {
+      return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
+  
+
+  // main.js
+
+window.addEventListener("appinstalled", () => {
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
+  
